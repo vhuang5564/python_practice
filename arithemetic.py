936db2e05a72d8f4c24282c05ecf0acba9782c76
unknown <vhuang5564@gmail.com>
2021-09-24 22:54:26 -0700

    augmented assignment operator

--- arithemetic.py
+++ arithemetic.py
@@ -2,3 +2,10 @@
 print(10 / 3)
 # output is integer with double  division operator //
 print(10 // 3)
+
+x = 10
+
+x = x + 3 # x = 13
+
+## augmented assignemt operator. line 7 = line 9
+x += 3 # x = 16
